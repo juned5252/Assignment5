2bdab299eef5e2f8326b854e78cd434be3f66ec8
Juned Alam <juned5252@gmail.com>
2018-08-27 14:28:18 -0400

    added instance and statuc members for payments

--- src/nestedClasses/Payments.java
+++ src/nestedClasses/Payments.java
@@ -1,4 +1,30 @@
 package nestedClasses;
 
 public class Payments {
+
+
+    private long receiptNumber;
+    private double amount;
+    static String currency = "U.S Dollars";
+
+
+    public long getReceiptNumber() {
+        return receiptNumber;
+    }
+
+    public void setReceiptNumber(long receiptNumber) {
+        this.receiptNumber = receiptNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double setAmount() {
+        return amount;
+    }
+
+
+
+
 }
