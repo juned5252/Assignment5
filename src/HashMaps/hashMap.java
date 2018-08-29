3dabf494ddba13b20f9da135326532ab5a620e96
Juned Alam <juned5252@gmail.com>
2018-08-29 09:22:36 -0400

    added hashMap utilization

---
+++ src/HashMaps/hashMap.java
@@ -0,0 +1,29 @@
+package HashMaps;
+
+import java.time.temporal.ValueRange;
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashMap {
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> nameNage = new HashMap<String,Integer>();
+
+        nameNage.put("juned", 24);
+        nameNage.put("Ziyad",7);
+        nameNage.put("javid",17);
+        nameNage.put("kashfia", 15);
+
+        for (Map.Entry en: nameNage.entrySet()) {
+            System.out.println(en);
+        }
+
+
+
+
+
+    }
+
+}
+
